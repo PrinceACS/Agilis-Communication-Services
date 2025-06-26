d072c6d8b09d492e5f9f05d34791aafd27a72409
PrinceACS <princetiwari180@gmail.com>
2025-06-26 11:55:56 +0000

    fix: login modal url & del:vercel.json

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -22,7 +22,7 @@ const LoginModal = () => {
       <div className="flex flex-col gap-3 mt-4">
         {/* Dialer and IVR */}
         <Link
-          href="https://calls.agiliscommunications.com/DIALER/"
+          href="https://calls.agiliscommunications.com"
           target="_blank"
           className="bg-[#6495ED] hover:bg-[#4682B4] text-white py-2 rounded text-center"
         >
